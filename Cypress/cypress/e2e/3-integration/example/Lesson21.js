60120de836b59738d28a04ca4787ee7283473b0c
anhbui2975 <165993703+anhbui2975@users.noreply.github.com>
2024-04-05 14:21:35 +0700

    Initial commit of Cypress folder

---
+++ Cypress/cypress/e2e/3-integration/example/Lesson21.js
@@ -0,0 +1,28 @@
+describe('My First Test Suite', function()
+{
+it('My FirstTest case', function() {
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    cy.get('.search-keyword').type('ca')
+    cy.wait(2000)
+    cy.get('.product').should('have.length', 5)
+    cy.get('.product:visible').should('have.length', 4)
+
+    cy.get('.products').find('.product').should('have.length',4)
+    cy.get(':nth-child(3) > .product-action > button').click()
+    cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
+    cy.get('.products').find('.product').each(($el, index, $list) => {
+        const textVeg=$e1.find('h4.product-name').text()
+        if(textVeg.includes('Cashews'))
+            {
+                $e1.find.('button').click()
+            }
+    }
+    ))
+})
+
+
+
+
+
+
